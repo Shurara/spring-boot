274909664bc36cdb1029ec403bddbb982036899a
alexr <alexey.rykhalskiy@gmail.com>
2018-07-10 16:53:27 +0300

    initial

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,24 @@
 package hello;
 
+import hello.controller.MyPersistence;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MyPersistence build1() {
+        return new MyPersistence();
     }
 
     //@Bean
